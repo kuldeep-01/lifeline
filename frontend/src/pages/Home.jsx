ee1d87122ec09a80174683d2d82cedbaaf98ec25
Kuldeep Singh <144509332+kuldeep-01@users.noreply.github.com>
2025-04-10 12:50:03 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from '../components/Header'
 import SpecialityMenu from '../components/SpecialityMenu'
-import TopDoctors from '../components/topdoctors'
+import TopDoctors from '../components/TopDoctors'
 import Banner from '../components/Banner'
 
 
